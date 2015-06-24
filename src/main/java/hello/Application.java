87c21bad8e56ea478728b1e6dcf350fcf1473c18
Glenn Renfro <grenfro@pivotal.io>
2015-06-24 18:19:39 -0400

    JMX is turned off however we have created a IntegrationMBeanExporter bean

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,20 +1,19 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.integration.IntegrationAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource("/integration.xml")
+@EnableAutoConfiguration(exclude = {JmxAutoConfiguration.class, IntegrationAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class);
-//		ConfigurableApplicationContext ctx = new SpringApplication("/integration.xml").run(args);
-//        System.out.println("Hit Enter to terminate");
-//        System.in.read();
-//        ctx.close();
     }
-
 }
